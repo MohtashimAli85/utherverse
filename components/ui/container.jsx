49976330701f13e088367cb8ae4cb5f492a62bf8
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-02 00:44:06 +0500

    Create container.jsx

---
+++ components/ui/container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container({ children }) {
+  return (
+    <div className="mx-4 sm:mx-6 md:max-w-5xl lg:mx-auto lg:max-w-6xl xl:max-w-7xl 2xl:max-w-full 2xl:mx-16">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
