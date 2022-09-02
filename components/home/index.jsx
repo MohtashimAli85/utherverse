7b0e80d9d566bed734e44fb4e55b75017ffcb8ff
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-02 21:53:27 +0500

    Update index.jsx

--- components/home/index.jsx
+++ components/home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "./hero-section";
 import MeetAeonSection from "./meet-aeon-section";
+import CardsSection from "./card-section";
 function Home() {
   return (
     <main className="space-y-24">
       <HeroSection />
       <MeetAeonSection />
+      <CardsSection />
     </main>
   );
 }
