248c6cc65f010ebedb1860becd07b8b253b0ef5d
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-02 10:23:40 +0500

    Create index.jsx

---
+++ components/home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "./hero-section";
+import MeetAeonSection from "./meet-aeon-section";
+function Home() {
+  return (
+    <main className="space-y-24">
+      <HeroSection />
+      <MeetAeonSection />
+    </main>
+  );
+}
+
+export default Home;
