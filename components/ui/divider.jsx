fa329f5b3458bca42e1bab2d92a87f518c24f8de
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-03 14:37:02 +0500

    Update divider.jsx

--- components/ui/divider.jsx
+++ components/ui/divider.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Image from "next/image";
-function Divider({ src, alt }) {
+function Divider({ src, alt, style }) {
   return (
-    <div className="divider text-center max-w-[117px] mx-auto mb-28">
+    <div
+      className={`divider text-center max-w-[117px] mx-auto mb-10 md:mb-28 ${style}`}
+    >
       <Image
         src={src}
         width={"100%"}
