288978dda41bcb703ff6557130d4f7066b14ecbb
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-03 14:37:05 +0500

    Create index.jsx

---
+++ components/home/roadmap/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RoadMap() {
+  return (
+    <section>
+      <h1 className="text-gradient-purple ">ROADMAP</h1>
+    </section>
+  );
+}
+
+export default RoadMap;
