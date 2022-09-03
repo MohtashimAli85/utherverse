65a3140313dddc691c0139de865854f364603124
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-03 19:52:59 +0500

    Update container.jsx

--- components/ui/container.jsx
+++ components/ui/container.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-function Container({ children }) {
+function Container({ children, style }) {
   return (
-    <section className="mx-4 sm:mx-6 md:max-w-5xl lg:mx-auto lg:max-w-6xl xl:max-w-7xl 2xl:max-w-full 2xl:mx-16">
+    <section
+      className={`${style} mx-4 sm:mx-6 md:max-w-5xl lg:mx-auto lg:max-w-6xl xl:max-w-6xl 2xl:max-w-[1356px]`}
+    >
       {children}
     </section>
   );
