0acd23b57e5327ca84c48b42fb9d76ba1d656fc2
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-04 13:49:48 +0500

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -6,7 +6,7 @@ import Container from "../ui/container";
 function Footer() {
   const allLinks = [
     {
-      name: "Vison",
+      name: "Vision",
       links: [
         { name: "Mission", href: "/" },
         { name: "Vision", href: "/" },
@@ -50,7 +50,6 @@ function Footer() {
       links: [
         { name: "Contact ", href: "/" },
         { name: "Follow us", href: "/" },
-        { name: "Disclaimer", href: "/" },
         { social: <SocialLinks /> },
       ],
     },
@@ -61,37 +60,52 @@ function Footer() {
     { name: "Terms and Conditions", href: "/" },
   ];
   return (
-    <footer>
-      <Container>
-        <section>
-          <div>
+    <footer className="mt-20 py-12 px-6">
+      <Container style="space-y-10">
+        <section className="flex gap-10 items-center w-full md:w-11/12 mx-auto ">
+          <div className="bg-dark-purple md:basis-9/12 px-5 py-10 rounded-2xl flex gap-4 flex-wrap">
+            <h2 className="text-xl md:text-2xl md:basis-1/2">
+              Subscribe to our newsletter and receive updates!
+            </h2>
+            <div className="md:basis-1/2 flex flex-wrap gap-4 items-center">
+              <input
+                type="email"
+                placeholder="Enter e-Mail"
+                className="bg-primary basis-full md:basis-9/12 p-3 rounded-md placeholder:text-white outline-none"
+              />
+              <button className="basis-full md:basis-1/4 bg-purple-monster hover:bg-purple-monster/75 transition duration-200 p-3 rounded-md">
+                SEND
+              </button>
+            </div>
+          </div>
+          <div className="basis-1/4 md:block hidden">
             <Image
               src="/logo-secondary.png"
               alt="logo"
-              width={100}
-              height={100}
+              width={300}
+              height={270}
+              className=""
             />
           </div>
-          <div>
-            <h2>Subscribe to our newsletter and receive updates!</h2>
-            <div>
-              <input type="email" placeholder="Enter e-Mail" />
-              <button>SEND</button>
-            </div>
-          </div>
         </section>
-        <section>
+        <section className="flex justify-between flex-col gap-3 sm:flex-row flex-wrap">
           {allLinks.map((e, i) => {
             return (
-              <div key={i} className="">
-                <h1>{e.name}</h1>
-                <ul>
+              <div key={i} className="space-y-1 basis1/4">
+                <h1 className="text-lg md:text-xl lg:text-2xl font-medium">
+                  {e.name}
+                </h1>
+                <ul className="space-y-2 font-light text-base md:text-lg">
                   {e.links.map((link, i) => {
                     return link?.social ? (
                       link.social
                     ) : (
-                      <li key={i}>
-                        <Link href={link?.href}>{link?.name}</Link>
+                      <li key={i} className="">
+                        <Link href={link?.href}>
+                          <a className="hover:text-primary transition duration-200">
+                            {link?.name}
+                          </a>
+                        </Link>
                       </li>
                     );
                   })}
@@ -100,8 +114,8 @@ function Footer() {
             );
           })}
         </section>
-        <section>
-          <p>
+        <section className="text-center space-y-5">
+          <p className="text-sm text-justify sm:text-center text-grey font-light md:w-10/12 mx-auto">
             **The information presented on this web page and throughout this web
             site is currently a work in progress. We make no assurances that it
             is error free, and there may be oversights while we build and proof
@@ -111,10 +125,15 @@ function Footer() {
             companies. For additional information please see our Privacy Policy
             and Terms and Conditions.
           </p>
-          <p>© 2022 Utherverse.io. All Rights Reserved.</p>
-          <ul>
+          <p className="font-light tracking-wide">
+            © 2022 Utherverse.io. All Rights Reserved.
+          </p>
+          <ul className="flex flex-wrap gap-4 justify-center">
             {specialLinks.map((link, i) => (
-              <li key={i} className="">
+              <li
+                key={i}
+                className="gradient text-sm font-medium px-2 py-1 rounded-md"
+              >
                 <Link href={link.href}>{link.name}</Link>
               </li>
             ))}
