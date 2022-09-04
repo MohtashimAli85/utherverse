1ab38603ce91bd29cf6fedf408a55ed355ea167e
Mohtashim Ali <mohtashima85@gmail.com>
2022-09-04 14:04:37 +0500

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -60,20 +60,20 @@ function Footer() {
     { name: "Terms and Conditions", href: "/" },
   ];
   return (
-    <footer className="mt-20 py-12 px-6">
+    <footer className="mt-20 py-12 md:px-6">
       <Container style="space-y-10">
         <section className="flex gap-10 items-center w-full md:w-11/12 mx-auto ">
-          <div className="bg-dark-purple md:basis-9/12 px-5 py-10 rounded-2xl flex gap-4 flex-wrap">
+          <div className="bg-dark-purple md:basis-9/12 px-5 py-10 rounded-2xl flex gap-4 flex-col md:flex-row">
             <h2 className="text-xl md:text-2xl md:basis-1/2">
               Subscribe to our newsletter and receive updates!
             </h2>
-            <div className="md:basis-1/2 flex flex-wrap gap-4 items-center">
+            <div className="md:basis-1/2 flex flex-col md:flex-row gap-4 items-center">
               <input
                 type="email"
                 placeholder="Enter e-Mail"
-                className="bg-primary basis-full md:basis-9/12 p-3 rounded-md placeholder:text-white outline-none"
+                className="bg-primary w-full md:basis-9/12 p-3 rounded-md placeholder:text-white outline-none"
               />
-              <button className="basis-full md:basis-1/4 bg-purple-monster hover:bg-purple-monster/75 transition duration-200 p-3 rounded-md">
+              <button className="w-full basis-full md:basis-1/4 bg-purple-monster hover:bg-purple-monster/75 transition duration-200 p-3 rounded-md">
                 SEND
               </button>
             </div>
@@ -115,6 +115,15 @@ function Footer() {
           })}
         </section>
         <section className="text-center space-y-5">
+          <div className="md:hidden">
+            <Image
+              src="/logo-secondary.png"
+              alt="logo"
+              width={200}
+              height={170}
+              className=""
+            />
+          </div>
           <p className="text-sm text-justify sm:text-center text-grey font-light md:w-10/12 mx-auto">
             **The information presented on this web page and throughout this web
             site is currently a work in progress. We make no assurances that it
